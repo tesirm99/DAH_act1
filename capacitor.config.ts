1c434b55937e49e136993d1819547119026e05c8
trm187 <trm187@inlumine.ual.es>
2023-03-10 18:46:08 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'act1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
